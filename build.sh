12973115ed714ad2803af66dba954bef996ee46e
vsalnitsky-art <vsalnitsky@gmail.com>
2025-12-05 16:24:02 +0000

    1

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Installing dependencies with pip..."
-pip install --upgrade pip setuptools wheel
-pip install -r requirements.txt
-
-echo "✅ Dependencies installed successfully!"
-
